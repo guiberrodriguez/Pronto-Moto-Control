772dabc8aeb0dcdf1ef4dbc96ad409eed6affa09
guiberrodriguez <guiber@prontopago.do>
2026-05-02 14:51:57 -0400

    Crear sw.js

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "pronto-moto-v1";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/logo.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
