3ab01db49fabf4f4424251efa0798be78b1c7b13
guiberrodriguez <guiber@prontopago.do>
2026-05-04 08:18:30 -0400

    Crear firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification?.title || "Pronto Moto", {
+    body: payload.notification?.body || "Nueva notificación"
+  });
+});
