5f62eaebc7a27676fdcdbd52e4a114ca2db1ea4a
guiberrodriguez <guiber@prontopago.do>
2026-05-04 07:24:19 -0400

    Actualizar sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,20 @@
-const CACHE_NAME = "pronto-moto-v1";
+const CACHE_NAME = "pronto-moto-enterprise-v1";
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.addAll([
         "/",
         "/index.html",
         "/manifest.json",
         "/logo.png"
-      ]);
-    })
+      ])
+    )
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
