0af3d5b3b79136c3e7e2785abd2eef9e2f6390c3
guiberrodriguez <guiber@prontopago.do>
2026-05-06 09:15:55 -0400

    Crear constants.js

---
+++ src/data/constants.js
@@ -0,0 +1,236 @@
+export const BASE_URL = window.location.origin;
+
+export const paises = [
+  "República Dominicana",
+  "República de Haití",
+  "Venezuela",
+  "Colombia",
+  "Cuba",
+  "Puerto Rico",
+  "Estados Unidos",
+  "México",
+  "España",
+  "Argentina",
+  "Chile",
+  "Perú",
+  "Ecuador",
+  "Brasil",
+  "Panamá",
+  "Costa Rica",
+  "Nicaragua",
+  "Honduras",
+  "El Salvador",
+  "Guatemala",
+  "Uruguay",
+  "Paraguay",
+  "Bolivia",
+  "Canadá",
+  "Francia",
+  "Italia",
+  "Alemania",
+  "Reino Unido",
+  "China",
+  "Japón",
+  "Corea del Sur"
+];
+
+export const nacionalidades = [
+  "Dominicana",
+  "Haitiana",
+  "Venezolana",
+  "Colombiana",
+  "Cubana",
+  "Puertorriqueña",
+  "Estadounidense",
+  "Mexicana",
+  "Española",
+  "Argentina",
+  "Chilena",
+  "Peruana",
+  "Ecuatoriana",
+  "Brasileña",
+  "Panameña",
+  "Costarricense",
+  "Nicaragüense",
+  "Hondureña",
+  "Salvadoreña",
+  "Guatemalteca",
+  "Uruguaya",
+  "Paraguaya",
+  "Boliviana",
+  "Canadiense",
+  "Francesa",
+  "Italiana",
+  "Alemana",
+  "Británica",
+  "China",
+  "Japonesa",
+  "Surcoreana"
+];
+
+export const provinciasRD = {
+  "Distrito Nacional": ["Santo Domingo de Guzmán"],
+  "Santo Domingo": [
+    "Santo Domingo Este",
+    "Santo Domingo Norte",
+    "Santo Domingo Oeste",
+    "Boca Chica",
+    "Los Alcarrizos",
+    "Pedro Brand",
+    "San Antonio de Guerra"
+  ],
+  "Santiago": [
+    "Santiago de los Caballeros",
+    "Bisonó",
+    "Jánico",
+    "Licey al Medio",
+    "Puñal",
+    "Sabana Iglesia",
+    "San José de las Matas",
+    "Tamboril",
+    "Villa González"
+  ],
+  "La Vega": ["La Vega", "Constanza", "Jarabacoa", "Jima Abajo"],
+  "San Cristóbal": [
+    "San Cristóbal",
+    "Bajos de Haina",
+    "Cambita Garabitos",
+    "Los Cacaos",
+    "Sabana Grande de Palenque",
+    "San Gregorio de Nigua",
+    "Villa Altagracia",
+    "Yaguate"
+  ],
+  "Puerto Plata": [
+    "Puerto Plata",
+    "Altamira",
+    "Guananico",
+    "Imbert",
+    "Los Hidalgos",
+    "Luperón",
+    "Sosúa",
+    "Villa Isabela",
+    "Villa Montellano"
+  ],
+  "San Pedro de Macorís": [
+    "San Pedro de Macorís",
+    "Consuelo",
+    "Guayacanes",
+    "Quisqueya",
+    "Ramón Santana",
+    "San José de los Llanos"
+  ],
+  "La Romana": ["La Romana", "Guaymate", "Villa Hermosa"],
+  "La Altagracia": ["Higüey", "San Rafael del Yuma"],
+  "Duarte": [
+    "San Francisco de Macorís",
+    "Arenoso",
+    "Castillo",
+    "Eugenio María de Hostos",
+    "Las Guáranas",
+    "Pimentel",
+    "Villa Riva"
+  ],
+  "Espaillat": [
+    "Moca",
+    "Cayetano Germosén",
+    "Gaspar Hernández",
+    "Jamao al Norte"
+  ],
+  "Peravia": ["Baní", "Nizao"],
+  "Azua": [
+    "Azua de Compostela",
+    "Estebanía",
+    "Guayabal",
+    "Las Charcas",
+    "Las Yayas de Viajama",
+    "Padre Las Casas",
+    "Peralta",
+    "Pueblo Viejo",
+    "Sabana Yegua",
+    "Tábara Arriba"
+  ],
+  "Barahona": [
+    "Barahona",
+    "Cabral",
+    "El Peñón",
+    "Enriquillo",
+    "Fundación",
+    "Jaquimeyes",
+    "La Ciénaga",
+    "Las Salinas",
+    "Paraíso",
+    "Polo",
+    "Vicente Noble"
+  ],
+  "San Juan": [
+    "San Juan de la Maguana",
+    "Bohechío",
+    "El Cercado",
+    "Juan de Herrera",
+    "Las Matas de Farfán",
+    "Vallejuelo"
+  ],
+  "Monseñor Nouel": ["Bonao", "Maimón", "Piedra Blanca"],
+  "Monte Plata": [
+    "Monte Plata",
+    "Bayaguana",
+    "Peralvillo",
+    "Sabana Grande de Boyá",
+    "Yamasá"
+  ],
+  "Hermanas Mirabal": ["Salcedo", "Tenares", "Villa Tapia"],
+  "María Trinidad Sánchez": ["Nagua", "Cabrera", "El Factor", "Río San Juan"],
+  "Samaná": ["Samaná", "Las Terrenas", "Sánchez"],
+  "Hato Mayor": ["Hato Mayor del Rey", "El Valle", "Sabana de la Mar"],
+  "El Seibo": ["El Seibo", "Miches"],
+  "Monte Cristi": [
+    "Monte Cristi",
+    "Castañuela",
+    "Guayubín",
+    "Las Matas de Santa Cruz",
+    "Pepillo Salcedo",
+    "Villa Vásquez"
+  ],
+  "Valverde": ["Mao", "Esperanza", "Laguna Salada"],
+  "Dajabón": ["Dajabón", "El Pino", "Loma de Cabrera", "Partido", "Restauración"],
+  "Santiago Rodríguez": ["San Ignacio de Sabaneta", "Los Almácigos", "Monción"],
+  "Elías Piña": [
+    "Comendador",
+    "Bánica",
+    "El Llano",
+    "Hondo Valle",
+    "Juan Santiago",
+    "Pedro Santana"
+  ],
+  "Independencia": [
+    "Jimaní",
+    "Cristóbal",
+    "Duvergé",
+    "La Descubierta",
+    "Mella",
+    "Postrer Río"
+  ],
+  "Bahoruco": ["Neiba", "Galván", "Los Ríos", "Tamayo", "Villa Jaragua"],
+  "Pedernales": ["Pedernales", "Oviedo"],
+  "Sánchez Ramírez": ["Cotuí", "Cevicos", "Fantino", "La Mata"]
+};
+
+export const pasarelasPago = [
+  "Efectivo",
+  "Depósito",
+  "Transferencia bancaria",
+  "Azul",
+  "CardNet",
+  "PayPal",
+  "Stripe",
+  "Link de pago externo"
+];
+
+export const metodosDigitales = [
+  "Azul",
+  "CardNet",
+  "PayPal",
+  "Stripe",
+  "Link de pago externo"
+];
